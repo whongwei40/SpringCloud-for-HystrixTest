035e600b4acbe4e070efc59eb861cb4272f37ac2
dongjunzi <2468673403@qq.com>
2023-02-13 14:05:50 +0800

    老虎机第一版

---
+++ cloud-rabbitmq-test/src/test/java/Game.java
@@ -0,0 +1,125 @@
+import java.util.*;
+
+public class Game {
+    // TODO: 2023/2/13  有可能下两个6，那么中的话，就是倍数*个数 ,目前没有个数
+    static Scanner input = new Scanner(System.in);
+    static Random random = new Random();
+    static ArrayList<String> options = new ArrayList<String>();
+    //大小结果映射 小于12是小  大于12是大
+    static HashMap<Integer, Integer> integerIntegerHashMap = new HashMap<Integer, Integer>();
+    static ArrayList<String> selectedOptions = new ArrayList<String>();
+    static double balance = 0.0;
+    static double totalEarnings = 0.0;
+    static {
+        // 添加12个选项
+        //初始化大小映射关系
+        for (int i = 1; i <= 12; i++) {
+            options.add(String.valueOf(i));
+            integerIntegerHashMap.put(i,i);
+        }
+        for (int i = 13; i <= 24; i++) {
+            integerIntegerHashMap.put(i, i-12);
+
+        }
+    }
+    public static void main(String[] args) {
+        //初始化金额
+        rechargeGoldCoins();
+        // 游戏开始
+        while (true) {
+            //下注
+            bet();
+            // 开始游戏
+            System.out.println("游戏开始！");
+            gameMain();
+            // 显示余额和累计收益
+            System.out.println("当前余额为：" + balance);
+            System.out.println("累计收益为：" + totalEarnings);
+            // 游戏结束，清空选择选项列表
+            selectedOptions.clear();
+        }
+    }
+
+    /**
+     * 充值金币
+     * @param
+     * @return
+     * @author dongjunzi
+     * @date 2023/02/13 01:18
+     */
+    static public void rechargeGoldCoins(){
+        // 玩家充值金额
+        System.out.print("请输入充值金额：");
+        balance += input.nextDouble();
+    }
+
+    /**
+     * 下注
+     * @param
+     * @return
+     * @author dongjunzi
+     * @date 2023/02/13 01:23
+     */
+    static public void bet(){
+        if (balance <= 0) {
+            System.out.println("余额不足，请充值！");
+            return;
+        }
+        // 玩家选择选项
+        while (true) {
+            System.out.print("请选择任意数量的选项（输入0开始游戏）：");
+            String selectedOption = input.next();
+            if (selectedOption.equals("0")) {
+                break;
+            } else if (options.contains(selectedOption)) {
+                selectedOptions.add(selectedOption);
+                balance -= 1.0; // 消耗一个金额
+                System.out.println("你目前的下注是");
+                selectedOptions.forEach(System.out::println);
+            } else {
+                System.out.println("无效选项，请重新选择！");
+            }
+        }
+
+    }
+
+    /**
+     * 游戏主要流程判断逻辑
+     * @param
+     * @return
+     * @author dongjunzi
+     * @date 2023/02/13 01:51
+     */
+    static public void gameMain(){
+
+        int randomOption = random.nextInt(25); // 生成0-24之间的随机结果
+        if (randomOption == 0 || randomOption == 24) {
+            System.out.println("你被吃了");
+        } else {
+            Integer mapResult = integerIntegerHashMap.get(randomOption);
+            if (randomOption <= 12) {
+                System.out.println("结果是小" + mapResult);
+                if (selectedOptions.contains(String.valueOf(mapResult))) {
+                    double earnings = 8.0; // 小的1-6的收益倍数是8倍
+                    totalEarnings += earnings;
+                    balance += earnings;
+                    System.out.println("恭喜你中奖了，收益为：" + earnings);
+                } else {
+                    System.out.println("很遗憾，你没有中奖。");
+                }
+            } else {
+                System.out.println("结果是大" + mapResult);
+                if (selectedOptions.contains(String.valueOf(mapResult))) {
+                    double earnings = 20.0; // 大的1-6的收益倍数是20倍
+                    totalEarnings += earnings;
+                    balance += earnings;
+                    System.out.println("恭喜你中奖了，收益为：" + earnings);
+                } else {
+                    System.out.println("很遗憾，你没有中奖。");
+                }
+            }
+        }
+
+    }
+
+}
